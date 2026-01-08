6f8b1bb0c49d88594d61b645219c6c2e3ba6243c
sayandeep sarkar <sarkarsayandeep093@gmail.com>
2026-01-08 23:26:45 +0530

    Complete clean ML DVC pipeline v2

--- src/prepare.py
+++ src/prepare.py
@@ -6,6 +6,6 @@ os.makedirs("data/processed", exist_ok=True)
 df = pd.read_csv("data/raw/data.csv")
 
 # preprocessing
-df["feature2"] = df["feature2"] * 2
+df["feature2"] = df["feature2"] * 3
 
 df.to_csv("data/processed/processed.csv", index=False)
